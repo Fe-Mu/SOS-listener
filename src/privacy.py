238c8470e2b9781a4ca2997223a22caa4b1577cf
Fe-Mu <lfgm.inbox@protonmail.com>
2025-06-19 22:21:54 +0200

    Add files via upload

---
+++ src/privacy.py
@@ -0,0 +1,96 @@
+
+"""
+
+SOS-listener is a multiplatform capable tool writen in Python/Kivy
+language and developed by L.F.G. Muñoz ("the Author") that offers
+the potential for SOS request in emergency situations.
+
+Copyright  (C)  2025  L.F.G. Muñoz  lfgm.copyright@protonmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
+
+title = """
+
+
+
+[u][b]Privacy Policy[/u][/b]
+"""
+
+txt_privacy = """
+
+
+
+This App (SOS-listener) does not share any private information with anyone. The only exception is when the end-user himself/herself/itself decides to share his/her/its current Location with a desired emergency contact or contacts.
+
+The information stored and used in this App is not encrypted and is stored only locally.
+"""
+
+txt_permissions = """
+[b]App's Permissions[/b]
+
+In order for the App to work properly it is necessary to grant three types of permissions:
+
+[i]Location[/i]
+[u]Purpose[/u]: To access the GPS location of the device in which this App is installed.
+
+[i]Telephone Calls[/i]
+[u]Purpose[/u]: To be able to call an emergency contact directly from the App.
+
+[i]Send SMS[/i]
+[u]Purpose[/u]: To be able to send SOS messages to an emergency contact directly from the App.
+"""
+
+txt_collection = """
+[b]Data collection[/b]
+
+Some functionalities of this App require the end-user to enter and save information in the app.
+
+[i]Location[/i]
+[u]Purpose[/u]: Provide the App’s functionality. The end-user’s current location is saved locally and is continuously updated with newer coordinates when the Location service in the phone is active and when the device’s Location permission has been previously granted. To disable access to the current end-user's position please turn off the Location service on the phone. This information is only stored locally.
+
+[i]Contact’s Name[/i]
+[u]Purpose[/u]: Provide the App’s functionality. This information is only stored locally.
+
+[i]Contact’s telephone number[/i]
+[u]Purpose[/u]: Provide the App’s functionality. This information is only stored locally.
+"""
+
+txt_usage = """
+[b]Data usage[/b]
+
+This App does not send and/or store private information to/in any server (or anywhere else).
+
+[i]Location[/i]
+[u]Purpose[/u]: in case the end-user decides to send an SOS message to a saved contact with his/her/its current location.
+
+[i]Contact’s Name[/i]
+[u]Purpose[/u]: For the end-user to be able to distinguish one contact person from other contact persons in the app.
+
+[i]Contact’s telephone number[/i]
+[u]Purpose[/u]: For the end-user to be able to call an emergency contact directly or to send an SOS message with his/her/its current location.
+"""
+
+txt_deletion = """
+[b]Data Deletion[/b]
+
+The Developer of this App has no means of accessing or deleting the information entered or saved in this App.
+
+The end-user can permanently delete the information stored in the app as following:
+    1. Contact person’s information: By deleting the contact and all its related information with the "Delete" button in the "Button Manager" window (refer to the How-To page for more information on how to do this) or by uninstalling the App.
+    2. Location: The last saved location will be permanently deleted by taping once on the button that is used to display GPS coordinates within the App or by uninstalling the App.
+
+"""
