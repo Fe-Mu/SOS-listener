1f6ecb1522ff812fe2604353f557d396bd8e710a
Fe-Mu <lfgm.inbox@protonmail.com>
2026-02-26 16:06:14 +0000

    Add files via upload
    Text Update

---
+++ src/about.py
@@ -0,0 +1,82 @@
+
+"""
+
+SOS-listener is a multiplatform capable tool writen in Python/Kivy
+language and developed by L.F.G. Muñoz ("the Author") that offers
+the potential for SOS request in emergency situations.
+
+Copyright  (C)  2025  L.F.G. Muñoz  sos.contact@protonmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
+
+title = """
+
+
+
+[u][b]About[/u][/b]
+"""
+
+txt_about ="""
+
+
+
+[b][u]IMPORTANT!!! - PLEASE READ!!![/u][/b]
+
+
+ [u][i]Disclaimer:[/i][/u]
+
+  1. This is not a professional App and it does not provide or promise to deliver any professional service.
+  2. This App is not guaranteed to function at any moment as intended. There are too many potential reasons, from hardware to software, why this App could fail to perform as intended, and therefore it can not be considered as a reliable source of help in an emergency situation. No end-user should rely solely on this App to provide or request aid, rescue or help in any given case or situation.
+  3. This App comes with no warranty of any kind. The use of this App is at the end-user's own risk.
+  4. This is not an App intended to be used as a phone contact book. This App was developed to be a potential source of help in case of an emergency. Use this App as intended, otherwise it could and might increase the probability of undesired results to happen.
+
+SOS-listener requires ALL three permissions (LOCATION, CALL_PHONE, SEND_SMS) in order to be able to work properly.
+
+[u][i]After the permissions have been granted, the App needs to be closed and [b]manually[/b] restarted to be able to initialize the GPS with the new configuration.[/i][/u]
+
+This is only needed to be done once. After the first restart the App should retain the granted permissions for future use.
+
+If you accidentally missed/forgot to grant all necessary permissions you can do the following:
+    
+  Option 1:
+    - Close the App
+    - Restart the App
+    - Grant all three permissions on App start
+    - If the permission popup do not come back then perform the steps explained in Option 2
+    
+  Option 2:
+    - Uninstall the App
+    - Reinstall the App
+    - Start App
+    - Grant all three permissions on App start
+    
+Additionally make sure that the GPS on your phone is turned on. This can be achieved by toggling on the Location switch in the phone Settings or by tapping the Location icon on your phone.
+
+If this is not done beforehand, even if the GPS permissions were granted, the App [b]WILL NOT[/b] work properly because the GPS will be off.
+
+
+[u][b]NOTE:[/b][/u]
+
+Please be aware that additional charges from your telephone service provider could be incurred when sending SOS messages (SMS) or calling emergency contacts.
+
+More information about this App can be found on:
+
+    https://github.com/Fe-Mu/SOS-listener
+
+
+"""
+
